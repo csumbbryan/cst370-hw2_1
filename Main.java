985b3459edb85d5abdd028556d8d91fc65656c2e
csumbbryan <bzanoli@csumb.edu>
2024-09-08 19:13:15 -0700

    Added pair handling functionality.

--- Main.java
+++ Main.java
@@ -20,20 +20,53 @@ import java.util.Scanner;
 
 class Main
 {
+    public static class Pairs {
+        public int x;
+        public int y;
+        public int distance;
+
+        public Pairs(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.distance = Math.abs(x - y);
+        }
+    }
+
     public static void main(String[] args) {
 
         // Develop your program here.
         // The following is just a sample statement and and you need to replace it with your code
         List<String> values = new ArrayList<String>();
+        List<Pairs> pairs = new ArrayList<Pairs>();
+        int curDistance = 0;
+        int minDistance = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String s1 = scanner.nextLine();
             values.add(s1);
         }
 
-        for (String value : values) {
-            System.out.println(value);
+        for (int i = 0; i < values.size(); i++) {
+            for (int k = 0; k < values.size(); k++) {
+                if (i != k) {
+                    int value1 = Integer.parseInt(values.get(i));
+                    int value2 = Integer.parseInt(values.get(k));
+                    curDistance = Math.abs(value1 - value2);
+                    if (curDistance < minDistance) {
+                        minDistance = curDistance;
+                    }
+                    if (value1 < value2) {
+                        pairs.add(new Pairs(value1, value2));
+                    }
+                    else {
+                        pairs.add(new Pairs(value2, value1));
+                    }
+                }
+            }
         }
+
+        System.out.println(pairs);
     }
 }
 
