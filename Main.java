b45802ae80b49d290ee7f806283589ffac05c5d0
csumbbryan <bzanoli@csumb.edu>
2024-09-08 19:15:29 -0700

    Added printing for pairs functionality.

--- Main.java
+++ Main.java
@@ -58,15 +58,17 @@ class Main
                     }
                     if (value1 < value2) {
                         pairs.add(new Pairs(value1, value2));
+                        System.out.println("Pair: " + value1 + " " + value2 + ", Distance: " + curDistance);
                     }
                     else {
                         pairs.add(new Pairs(value2, value1));
+                        System.out.println("Pair: " + value1 + " " + value2 + ", Distance: " + curDistance);
                     }
                 }
             }
         }
 
-        System.out.println(pairs);
+        //System.out.println(pairs);
     }
 }
 
